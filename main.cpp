529ee90e6f1a62666a337af43876737b132c15ea
gcaballero1 <gcaballero1@mail.csuchico.edu>
2018-04-17 11:42:36 -0700

    Rearrange values

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
+//Author: Gabriel Caballero
 #include <iostream>
 using namespace std;
+
+void rearrangeColors(int&, int&, int&);
 
 int main()
 {
@@ -9,7 +12,7 @@ int main()
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
 
-
+  rearrangeColors(red,green,blue);
 
 
 
@@ -18,4 +21,55 @@ int main()
   cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
   return 0;
   //...END OF "DO NOT CHANGE" AREA
+}
+
+void rearrangeColors(int&R, int&G,int&B)
+{
+int greatest;
+int mid;
+int least;
+
+if (R>=G && R>=B && G>=B)
+{
+greatest = R;
+mid = G;
+least = B;
+}
+else if (R>=G && R>=B && B>=G)
+{
+greatest = R;
+mid = B;
+least = G;
+}
+else if (G>=R && G>=B && R>=B)
+{
+greatest = G;
+mid = R;
+least = B;
+}
+else if (G>=R && B>=B && B>=R)
+{
+greatest = G;
+mid = B;
+least = R;
+}
+else if (B>=R && B>=G && G>=R)
+{
+greatest = B;
+mid = G;
+least = R;
+}
+else if (B>=R && B>=G && R>=G)
+{
+greatest = B;
+mid = R;
+least = G;
+}
+else
+{
+cout<<"ERROR\n";
+}
+R=greatest;
+G=mid;
+B=least;
 }
